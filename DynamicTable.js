6bb8aa8ae424428f4d18ae1f6c40011b18679d41
Jonathan Ritchey <jonathan@kurbo.com>
2015-11-26 16:09:29 -0800

    start refactoring

--- DynamicTable.js
+++ DynamicTable.js
@@ -1,74 +1,57 @@
+function drawDynamicTable() {
+}
 function* algorithm() {
     
-    width = 110;
-    height = 50;
-    rows = 6
-    columns = 6
+    cellWidth = 110;
+    cellHeight = 50;
     gridColor = new algo.Color({ red: 0x78/255.0, green: 0x92/255.0, blue: 0xB2/255.0, alpha: 1})
     clearColor = new algo.Color({ red: 0.5, green: 0.5, blue: 1, alpha: 0})
     fontSize = 12
     
-    // var box = new algo.layout.Box(width/4, height/4, (columns+2)*width-width/4, (rows+2)*height-height/4);
-    // new algo.render.Arrow({
-    //     shape: algo.layout.Line.topEdge(box),
-    //     startArrow: false,
-    //     endArrow: true,
-    //     inset: width
-    // });
-    // new algo.render.Arrow({
-    //     shape: algo.layout.Line.leftEdge(box),
-    //     startArrow: false,
-    //     endArrow: true,
-    //     inset: height
-    // });
-    // var columnTitleBox = new algo.render.Rectangle({
-    //     x:width*2,
-    //     y:0,
-    //     w:width*2,
-    //     h:height/4,
-    //     stroke:clearColor,
-    //     fontSize: fontSize,
-    //     text:"column name"
-    // })
-    
     var columnGrid = new algo.render.ElementGroup();
     var rowGrid = new algo.render.ElementGroup();
     var grid = new algo.render.ElementGroup();
-
-    tableTitleHeight = height/3
-    tableX = width
-    tableY = tableTitleHeight
+    var rowNames = ["a","ab","abd","abde","abdez"]
+    var columnNames = ["a","ab","abc","abcd","abcde","abcdef"]
+    var rows = rowNames.length
+    var columns = columnNames.length
+    
+    tableColumnTitleHeight = cellHeight/3
+    tableRowTitleWidth = cellWidth*0.7
+    tableX = 10
+    tableY = 0
     for(i = 0; i < rows; i++ ) {
         rowGrid.add(new algo.render.Rectangle({
-            x: 0,
-            y: tableY + i * height,
-            w: width,
-            h: height,
+            x: tableX,
+            y: tableY + tableColumnTitleHeight + i * cellHeight,
+            w: cellWidth,
+            h: cellHeight,
             stroke: clearColor,
             fill: clearColor,
-            text: "row "+i,
+            text: rowNames[i],
+            textAlign:"left",
             fontSize: fontSize
         }));
     }
     for(j = 0; j < columns; j++ ) {
         columnGrid.add(new algo.render.Rectangle({
-            x: tableX + (j * width),
-            y: 0,
-            w: width,
-            h: tableTitleHeight,
+            x: tableX + tableRowTitleWidth + j * cellWidth,
+            y: tableY,
+            w: cellWidth,
+            h: tableColumnTitleHeight,
             stroke: clearColor,
             fill: clearColor,
-            text: "column "+j,
+            text: columnNames[j],
             fontSize: fontSize
         }));
     }
     for(i = 0; i < rows; i++ ) {
         for(j = 0; j < columns; j++ ) {
         grid.add(new algo.render.Rectangle({
-            x: tableX + j * width,
-            y: tableY + i * height,
-            w: width,
-            h: height,
+            x: tableX + tableRowTitleWidth + j * cellWidth,
+            y: tableY + tableColumnTitleHeight + i * cellHeight,
+            w: cellWidth,
+            h: cellHeight,
             stroke: gridColor,
             fill: clearColor,
             text: "row: "+i+" col: "+j+"very long text for line warp",//"multiline\n/line2\n/line3",
