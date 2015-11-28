d7a8fe3ee8ae827e17844359613d36088af6fd88
Jonathan Ritchey <jonathan@kurbo.com>
2015-11-27 20:42:01 -0800

    fix colors & add code highlights

--- DynamicTable.js
+++ DynamicTable.js
@@ -1,14 +1,15 @@
 function* algorithm() {
-    setHighlightColor = new algo.Color({ red: 0x78/255.0, green: 0x92/255.0, blue: 0xB2/255.0, alpha: 0.2})
-    getHighlightColor = new algo.Color({ red: 0x78/255.0, green: 0x92/255.0, blue: 0xB2/255.0, alpha: 0.1})
-    answerHighlightColor = new algo.Color({ red: 0/255.0, green: 0x76/255.0, blue: 0xFF/255.0, alpha: 0.5})
+    setHighlightColor = new algo.Color({ red: 0x55/255.0, green: 0xAA/255.0, blue: 0x55/255.0, alpha: 0.2})
+    getHighlightColor = new algo.Color({ red: 0x44/255.0, green: 0x92/255.0, blue: 0xCC/255.0, alpha: 0.2})
+    answerHighlightColor = new algo.Color({ red: 0x66/255.0, green: 0xAA/255.0, blue: 0x66/255.0, alpha: 0.4})
+    //new algo.Color({ red: 0/255.0, green: 0x76/255.0, blue: 0xFF/255.0, alpha: 0.4})
     var source = "abcdef";
     var target = "azced";
     var columnNames = prefixesForString(source);
     var rowNames = prefixesForString(target);
     rows = rowNames.length;
     columns = columnNames.length;
-    cellWidth = 110;
+    cellWidth = 120;
     cellHeight = 50;
     tableX = 10;
     tableY = 0;
@@ -58,6 +59,7 @@ function* algorithm() {
         text = ""+i+" edits from "+columnNames[0] + " → "+rowNames[i]+".";
         setGridAtRowColumnToText(grid,i,0,columns,text,setHighlightColor);
         evalLines[2]="T["+i+"][0] = "+i
+        highlightCodeLineInGrid(evalGrid,0,2,setHighlightColor)
         replaceCodeLinesInGrid(evalGrid,evalLines,0,2)
         yield({step:text});
         setGridAtRowColumnToText(grid,i,0,columns,""+i);
@@ -70,6 +72,7 @@ function* algorithm() {
         setGridAtRowColumnToText(grid,0,j,columns,text,setHighlightColor);
         evalLines[4]="T[0]["+j+"] = "+j
         replaceCodeLinesInGrid(evalGrid,evalLines,3,4)
+        highlightCodeLineInGrid(evalGrid,3,4,setHighlightColor)
         yield({step:text})
         setGridAtRowColumnToText(grid,0,j,columns,""+j,setHighlightColor);
         unhighlightGrid(grid);
@@ -85,6 +88,7 @@ function* algorithm() {
                 actualEdits = getGridTextAtRowColumn(grid,i-1,j-1,columns);
                 evalLines[10] = "T["+i+"]["+j+"]="+actualEdits
                 replaceCodeLinesInGrid(evalGrid,evalLines,5,13)
+                highlightCodeLineInGrid(evalGrid,5,10,setHighlightColor)
                 text = "last char same, so "+actualEdits+" edits.";
                 setGridAtRowColumnToText(grid,i,j,columns,text,setHighlightColor);
                 yield({step:text})
@@ -98,6 +102,7 @@ function* algorithm() {
                 actualEdits = 1 + Math.min(left,upperLeft,rowAbove);
                 evalLines[13] = "T["+i+"]["+j+"]="+actualEdits
                 replaceCodeLinesInGrid(evalGrid,evalLines,5,13)
+                highlightCodeLineInGrid(evalGrid,5,13,setHighlightColor)
                 text = "1 + min("+left+","+upperLeft+","+rowAbove+") edits.";
                 setGridAtRowColumnToText(grid,i,j,columns,text,setHighlightColor);
                 yield({step:text})
@@ -144,10 +149,18 @@ function replaceCodeLinesInGrid(grid,codeLines,startIndex,endIndex) {
     }
 }
 
+function highlightCodeLineInGrid(grid,startIndex,absoluteIndex,highlightColor) {
+    clearColor = new algo.Color({ red: 0.5, green: 0.5, blue: 1, alpha: 0})
+    grid.set({fill:clearColor})
+    grid.elements[absoluteIndex-startIndex].set({
+        fill:highlightColor
+    })
+}
+
 function makeDynamicTable(rowNames,columnNames,cellWidth,cellHeight,tableX,tableY) {
     gridColor = new algo.Color({ red: 0x78/255.0, green: 0x92/255.0, blue: 0xB2/255.0, alpha: 1})
     clearColor = new algo.Color({ red: 0.5, green: 0.5, blue: 1, alpha: 0})
-    fontSize = 12
+    fontSize = 14
     
     var columnGrid = new algo.render.ElementGroup();
     var rowGrid = new algo.render.ElementGroup();
